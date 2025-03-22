b93c2930ce39654a385b70981f628e62d8e635b8
AdelHANIFI <adel.hanifi@imerir.com>
2025-03-22 09:28:31 +0100

    mise à jour automatique 2025-03622 09:28:31

---
+++ update_kiosque.sh
@@ -0,0 +1,14 @@
+cd /home/mosqueePau/Desktop/Kiosque 
+
+. myenv/bin/activate
+
+git add .
+git commit -m "mise à jour automatique $(date '+%Y-%m6%d %H:%M:%S')"
+
+git push origin main
+
+git pull origin main
+
+pkill -f main.py 
+python main.py &
+
