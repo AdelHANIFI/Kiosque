4c755d191f87ca5db0cc70054660bf2aa942ade1
adel <adel.hanifi@imerir.com>
2025-03-26 14:27:52 +0100

    modification .sh

--- update_kiosque.sh
+++ update_kiosque.sh
@@ -5,6 +5,10 @@ export DISPLAY=:0.0
 export XAUTHORITY=/home/mosqueePau/.Xauthority
 git config --global --add safe.directory "/home/mosqueePau/Desktop/Kiosque"
 git config --global credential.helper store 
+GIT_TOKEN = "ghp-PRyB9ZQidR7TWYkivZjHe7hnroxz1A1HUrQo"
+GIT_REPO="https://$GIT_TOKEN@github.com/AdelHANIFI/Kiosque.git"
+
+
 cd /home/mosqueePau/Desktop/Kiosque 
 
 
@@ -14,9 +18,9 @@ cd /home/mosqueePau/Desktop/Kiosque
 git add .
 git commit -m "mise à jour automatique $(date '+%Y-%m6%d %H:%M:%S')"
 
-git push origin main
+git push "$GIT_REPO" main
 
-git pull origin main
+git pull "$GIT_REPO" main
 
 sudo pkill -f main.py 
 python main.py &
